c4c599a807b3fad244ecbcec8b701b6db36a2cf1
DESKTOP-HU04ENR\estrategas <brandon10torres@gmail.com>
2017-10-16 11:50:10 -0500

    Prototipo de invitacion con errores de iluminación al cambiar de escena y error Error: Duplicate Files in APK

---
+++ PrototipoInvitacion/Assets/Scripts/UIControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour {
+
+    public MeshRenderer mallaCubo;
+    public Color colorClick;
+
+    private Color colorCubo;
+    private bool clickCubo = false;
+
+    public Animator animCubo;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        colorCubo = mallaCubo.material.color;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    public void ClickColorCubo()
+    {
+        clickCubo = !clickCubo;
+        if(clickCubo)
+        {
+            mallaCubo.material.color = colorClick;
+            animCubo.SetTrigger("Acercado");
+            animCubo.SetTrigger("Info");
+        }
+        else
+        {
+            colorCubo = mallaCubo.material.color;
+        }
+    }
+}
---
+++ PrototipoInvitacion/Assets/Scripts/customTrackableEvent.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.SceneManagement;
+
+public class customTrackableEvent : MonoBehaviour,
+                                                ITrackableEventHandler
+{
+
+
+    public TrackableBehaviour mTrackableBehaviour;
+    public MeshRenderer mallaCubo;
+    public Color colorCubo;
+
+
+
+    // Use this for initialization
+    void Start ()
+    {        
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnTrackableStateChanged(
+                                        TrackableBehaviour.Status previousStatus,
+                                        TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            OnTrackingFound();
+        }
+        else
+        {
+            OnTrackingLost();
+        }
+    }
+    private void OnTrackingFound()
+    {
+        mallaCubo.material.color = colorCubo;
+        print("Encontrado");
+    }
+    private void OnTrackingLost()
+    {
+        print("Perdido");
+    }
+    void OnMouseDown()
+    {
+        
+        SceneManager.LoadScene("Invitacion");               
+    }
+}
