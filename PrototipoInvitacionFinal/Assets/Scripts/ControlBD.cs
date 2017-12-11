cd4717623f6651c1fc8ec07e6b9a50c861bfa54c
DESKTOP-HU04ENR\estrategas <brandon10torres@gmail.com>
2017-12-11 15:08:30 -0600

    falta registro en BD

---
+++ PrototipoInvitacionFinal/Assets/Scripts/ControlBD.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlBD : MonoBehaviour 
+{
+	private string URL = "http://localhost/invitaciones/index.php";
+	//private string URL = "https://estrategasdigitales.com/InvitacionED/index.php";
+	public GameObject respuestaGO;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public IEnumerator ConectarBD(string nombreInvitado, string apellidoInvitado, string correoInvitado)
+	{
+		WWWForm form = new WWWForm ();
+
+		form.AddField ("nombre", nombreInvitado);
+		form.AddField ("apellido", apellidoInvitado);
+		form.AddField ("correo", correoInvitado);
+
+		WWW llamadaBD = new WWW (URL, form);
+		yield return llamadaBD;
+
+		if(!string.IsNullOrEmpty(llamadaBD.error))
+		{
+			Debug.Log ("ERROR" + llamadaBD.error);
+		}
+		respuestaGO.SendMessage ("respuestaBD", llamadaBD.text);
+	}
+}
---
+++ PrototipoInvitacionFinal/Assets/Scripts/ControlRegistro.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ControlRegistro : MonoBehaviour 
+{
+	public Text nombre;
+	public Text apellido;
+	public Text correo;
+	public Text error;
+	public ControlBD miBD;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		error.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	public void SalirMenu()
+	{
+		SceneManager.LoadScene ("Invitacion");
+	}
+	public void RegistrarInvitado()
+	{
+		if (string.IsNullOrEmpty (nombre.text) || string.IsNullOrEmpty (apellido.text) || string.IsNullOrEmpty (correo.text)) {
+			error.text = "Ingrese todos los campos";
+		} else 
+		{
+			StartCoroutine(miBD.ConectarBD(nombre.text, apellido.text, correo.text));
+		}			
+	}
+
+	void respuestaBD(string msj)
+	{
+		if (msj == "Ingresado") 
+		{
+			Debug.Log ("Cargar escena invitacion");
+			SceneManager.LoadScene ("Invitacion");
+		}
+	}
+}
